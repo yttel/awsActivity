f43c30f5630534a63af8d25853cce380d820e00e
EC2 Default User <ec2-user@ip-172-31-87-201.ec2.internal>
2020-04-19 14:30:46 +0000

    changes for vm to run

--- config/config.js
+++ config/config.js
@@ -3,26 +3,26 @@ require("dotenv").config();
 module.exports=
 {
   "development": {
-    "username": process.env.username,
-    "password": process.env.password,
-    "database": "database-1",
-    "host": process.env.host,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": "database1",
+    "host": process.env.DB_HOST,
     "port": 3306,
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": "database1",
+    "host": process.env.DB_HOST,
     "port": 3306,
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": "database1",
+    "host": process.env.DB_HOST,
     "port": 3306,
     "dialect": "mysql"
   }
